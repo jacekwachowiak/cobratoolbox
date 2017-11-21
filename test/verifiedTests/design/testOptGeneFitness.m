34115ffe31280dcbf083138e7cd1e4bbf16648cb
Jacek <jacekwachowiak94@gmail.com>
2017-11-21 12:08:34 +0100

    WIP

--- test/verifiedTests/design/testOptGeneFitness.m
+++ test/verifiedTests/design/testOptGeneFitness.m
@@ -1,7 +1,7 @@
-% The COBRAToolbox: testOptGeneFitnessTilt.m
+% The COBRAToolbox: testOptGeneFitness.m
 %
 % Purpose:
-%     - test the optGeneFitnessTilt function
+%     - test the optGeneFitness function
 %
 % Authors:
 %     - Jacek Wachowiak
@@ -10,7 +10,7 @@ global CBTDIR
 currentDir = pwd;
 
 % initialize the test
-fileDir = fileparts(which('testOptGeneFitnessTilt'));
+fileDir = fileparts(which('testOptGeneFitness'));
 cd(fileDir);
 
 % test variables
@@ -18,14 +18,12 @@ model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'mat' filese
 model.lb(36) = 0; % setting the model to anaerobic conditions
 model.ub(36) = 0; % setting the model to anaerobic conditions
 target = model.rxns(13);
-rxn_vector_matrix = 0;
-rxnListInput = model.rxns
+rxn_vector_matrix = [1];
+rxnListInput = model.rxns;
 isGeneList = 0;
 
-%reference data
-
 % function outputs
-[val] = optGeneFitnessTilt(rxn_vector_matrix, model, targetRxn, rxnListInput, isGeneList)
+[val] = optGeneFitness(rxn_vector_matrix, model, targetRxn, rxnListInput, isGeneList)
 
 % tests -
 assert(isequal(0, 0));
